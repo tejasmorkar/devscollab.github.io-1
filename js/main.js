a52da48c8f3d90459a790601a4516a71bf971537
Tejas Morkar <tejasmorkar@gmail.com>
2019-05-17 19:29:51 +0530

    Adding files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const newLink = (link) => {
+    var win = window.open(link, '_blank');
+    if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+    } else {
+        //Browser has blocked it
+        alert('Please allow popups for this website');
+    }
+} 
