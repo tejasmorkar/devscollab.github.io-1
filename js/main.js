d9d29362dbd395b9219062e6bf85c3b27a6232d9
suyashsonawane <suyashsonawane007@gmail.com>
2019-05-30 15:16:46 +0530

    adding dynamic content

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 // This function is called onclick and the url is sent as parameter link
 
-const newLink = (link) => {
-    let win = window.open(`https://` + link, '_blank');
-    if (win) {
-        //Browser has allowed it to be opened
-        win.focus();
-    } else {
-        //Browser has blocked it
-        alert('Please allow popups for this website');
-    }
-}
-
-
+const newLink = link => {
+  let win = window.open(`https://` + link, "_blank");
+  if (win) {
+    //Browser has allowed it to be opened
+    win.focus();
+  } else {
+    //Browser has blocked it
+    alert("Please allow popups for this website");
+  }
+};
 
 // Scroll Down and Scroll Up events
 
@@ -27,3 +25,32 @@ const newLink = (link) => {
 //    }
 //    lastScrollTop = st;
 // });
+
+
+// Getting infos about repos deom github api
+
+let repo_url = `https://api.github.com/users/devscollab/repos`
+async function getRepos() {
+  let response = await fetch(repo_url);
+  let data = await response.json()
+  return data;
+}
+
+
+
+// VUE JS
+var vm = new Vue({
+  el: '#app',
+  data: {
+    list: null,
+    desc: 'https://www.google.com'
+  },
+  created() {
+    getRepos()
+      .then(data => {
+        this.list = data
+
+        // console.log(data[0])
+      });
+  }
+})
--- js/members.js
+++ js/members.js
@@ -1,12 +1,41 @@
 // This function is called onclick and the url is sent as parameter link
 
-const newLink = (link) => {
-    let win = window.open(`https://` + link, '_blank');
-    if (win) {
-        //Browser has allowed it to be opened
-        win.focus();
-    } else {
-        //Browser has blocked it
-        alert('Please allow popups for this website');
-    }
+const newLink = link => {
+  let win = window.open(`https://` + link, "_blank");
+  if (win) {
+    //Browser has allowed it to be opened
+    win.focus();
+  } else {
+    //Browser has blocked it
+    alert("Please allow popups for this website");
+  }
+};
+
+
+// VUE JS
+
+let repo_url = `https://api.github.com/repos/devscollab/devscollab.github.io/contributors`
+async function getRepos() {
+  let response = await fetch(repo_url);
+  let data = await response.json()
+  return data;
 }
+
+
+
+// VUE JS
+var vm = new Vue({
+  el: '#app',
+  data: {
+    list: null,
+    desc: 'https://www.google.com'
+  },
+  created() {
+    getRepos()
+      .then(data => {
+        this.list = data
+
+        console.log(data)
+      });
+  }
+})
